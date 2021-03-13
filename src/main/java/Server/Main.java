dbac2e63fbd61db089e1a0161e0f2a8e7fcc1d81
Erdei Csongor <erdeicsongor2000@gmail.com>
2021-03-13 15:58:23 +0200

    Server

--- src/main/java/Server/Main.java
+++ src/main/java/Server/Main.java
@@ -5,11 +5,13 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Main {
 
     public static void main(String[] args) throws IOException, ParseException {
-        JSONParser parser = new JSONParser();
+        /*JSONParser parser = new JSONParser();
         FileReader file = new FileReader(".\\src\\main\\database.json");
 
         Object obj = parser.parse(file);
@@ -22,9 +24,30 @@ public class Main {
         System.out.println(Json.nodeToString(database));
         System.out.println(database.getTables().get(0).getName());
         System.out.println(database.getTables().get(1).getName());
-        System.out.println(database.getTables().get(0).getAttributes().get(0).getName());
+        System.out.println(database.getTables().get(0).getAttributes().get(0).getName());*/
 
+        int portNumber = 4242;
 
+        try {
+            ServerSocket serverSocket = new ServerSocket(portNumber);
+
+            while(true) {
+                Socket clientSocket = serverSocket.accept();
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+                String inputLine;
+                while ((inputLine = in.readLine()) != null) {
+                    System.out.println(inputLine);
+                    if (inputLine.equals("exit"))
+                        break;
+                }
+                break;
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
