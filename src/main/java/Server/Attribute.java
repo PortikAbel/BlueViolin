28b05887a18c479140d506704b9464eaaf619961
Erdei Csongor <erdeicsongor2000@gmail.com>
2021-03-13 15:52:01 +0200

    Server

--- src/main/java/Server/Attribute.java
+++ src/main/java/Server/Attribute.java
@@ -4,8 +4,13 @@ import org.json.simple.JSONObject;
 
 public class Attribute {
     private String name;
+    private boolean pK;
+    private boolean fK;
+    private boolean notNUll;
+
 
     public Attribute(JSONObject o) {
+
         this.name = (String) o.get("name");
     }
 
